98c5b0395b1586f671e936dde2f3881bf98a94c3
Dennis <dennistoledo314@gmail.com>
2024-11-29 18:34:11 -0500

    Updated test_v_morpho

--- test_pointelectrode.py
+++ test_pointelectrode.py
@@ -124,15 +124,11 @@ def test_origin_to_mid(elect_for_testing: PointElectrode, axon_morpho: Morpholog
 
 def test_v_morpho(elect_for_testing: PointElectrode, axon_morpho: Morphology) -> None:
     internode_length, paranode_length, node_length = 110 * um, 3 * um, 1 * um
-    v_actual = elect_for_testing.v_morpho(node_length, paranode_length, internode_length, -1, -1, axon_morpho)
-    # assert abs(v_actual[6]) >= abs(v_actual[0])
-    # assert abs(v_actual[6]) >= abs(v_actual[1])
-    # assert abs(v_actual[6]) >= abs(v_actual[2])
-    # assert abs(v_actual[6]) >= abs(v_actual[3])
-    # assert abs(v_actual[6]) >= abs(v_actual[4])
-    # assert abs(v_actual[6]) >= abs(v_actual[5])
-
-
+    v_actual1 = elect_for_testing.v_morpho(node_length, paranode_length, internode_length, -1, -1, axon_morpho)
+    v_actual2 = elect_for_testing.v_morpho(node_length, paranode_length, internode_length, 5, 1, axon_morpho)
+    v_actual = {**v_actual1, **v_actual2}
+    assert v_actual[1] == v_actual[3]
+    assert v_actual[0] == v_actual[4]
 
 
 
